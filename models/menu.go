0f665b3f915ba56b8581fc0c4b0144a00d0e6565
Vladyslav Pavlenko <vladyslavpavlen@gmail.com>
2024-05-02 15:15:47 +0300

    crud operations

--- models/menu.go
+++
@@ -1,9 +0,0 @@
-package models
-
-// Menu is the menu model.
-type Menu struct {
-	ID           uint       `gorm:"primaryKey"`
-	RestaurantID uint       `gorm:"not null;index"`
-	Restaurant   Restaurant `gorm:"foreignKey:RestaurantID"`
-	Title        string     `gorm:"size:255;not null"`
-}
